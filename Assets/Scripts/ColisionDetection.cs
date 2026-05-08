c814e6e007570e49cccf0ad4c59923ac82952740
kiwito010 <justorodrip@gmail.com>
2026-05-08 14:33:30 -0300

    Initial commit

---
+++ Assets/Scripts/ColisionDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("hubo colision");
+        if (col.gameObject.CompareTag("Destroy")){
+            Destroy(col.gameObject);
+        }
+        else if(col.gameObject.CompareTag("Piso")){
+        Destroy(gameObject);
+        }
+    }
+}
